1967f5b4ecbd988aab3e41f2194d03e8ac24bdf4
Kristel Geraldine Villalta Porras <158532211+KrisGerald02@users.noreply.github.com>
2024-09-02 09:54:13 -0600

    Update Numeros_For_ContAcum.cs

--- Metodologia de Programacion Estructurada II Semestre/Numeros_For_ContAcum.cs
+++ Metodologia de Programacion Estructurada II Semestre/Numeros_For_ContAcum.cs
@@ -1,1 +1,23 @@
+using System;
 
+   public class Program
+   {
+      public static void Main(string[] args)
+      {
+      int cont;
+      float suma;
+
+      Console.Clear();
+
+      //Buenas practicas iniciar el contador y acumulador en 0
+      cont = 0;
+      suma = 0;
+      for(cont=0; cont <= 10; cont++)
+      { 
+         Console.WriteLine("Numero: {0}", cont);
+         suma += cont;
+      }
+      Console.WriteLine();
+      Console.ReadKey();
+      }
+   }
