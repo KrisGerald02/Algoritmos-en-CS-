c4a30f8949ee67756226d833ea9538cd53423253
Kristel Geraldine Villalta Porras <158532211+KrisGerald02@users.noreply.github.com>
2024-09-02 10:21:10 -0600

    Update ForEach.cs

--- Metodologia de Programacion Estructurada II Semestre/ForEach.cs
+++ Metodologia de Programacion Estructurada II Semestre/ForEach.cs
@@ -1,1 +1,21 @@
+using System;
 
+   public class Program
+   {
+      public static void Main(string[] args)
+      {
+      string[] nombres = { "Juan", "Pedro", "Luisa", "Amanda", "Carlos"};
+
+      Console.Clear();
+
+      //siempre se declara una variabe dentro
+      //Imprime la lista que contiene los elementos
+      foreach (var nombre in nombres)
+      {
+         //Con el {0} se le indica desde donde iniciara la iteracion
+         Console.WriteLine("Nombre: {0}", nombre);
+      }
+      Console.WriteLine();
+      Console.ReadKey();
+      }
+   }
