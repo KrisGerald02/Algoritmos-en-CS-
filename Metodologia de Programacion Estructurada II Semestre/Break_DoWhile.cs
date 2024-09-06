a2aaf426017968951da1f6fec2a6b41e5c260e42
Kristel Geraldine Villalta Porras <158532211+KrisGerald02@users.noreply.github.com>
2024-09-06 09:55:00 -0600

    Update Break_DoWhile.cs

--- Metodologia de Programacion Estructurada II Semestre/Break_DoWhile.cs
+++ Metodologia de Programacion Estructurada II Semestre/Break_DoWhile.cs
@@ -7,22 +7,41 @@ using System;
       int n;
       char resp; 
       Console.WriteLine("Uso de Entrada con Iteraciones (Si/No)");
-      n = int.Parse(Console.ReadLine());
 
       do
       {
          Console.WriteLine("Desea ingresar otro numero (S= Si || N= No): ");
          resp = char.Parse(Console.ReadLine());
+
+         //Para no
+         if (resp == 'N' || resp == 'n')
+          break;
+
          Console.WriteLine("Ingrese el numero: ");
          n = int.Parse(Console.ReadLine());
 
-         //Para no
-         if (resp == 'N' || resp == 'N')
-         resp = char.Parse(Console.ReadLine());
-         Console.WriteLine("Fin del Programa");
-            break;
-        
       } while (resp == 'S' || resp == 's');
+
+      //Centinela
+      Console.WriteLine("Uso de Centinela");
+      do
+      {
+         Console.WriteLine(" Ingrese un numero - (Digite -99 para finalizar): ");
+         n = int.Parse(Console.ReadLine());
+      } while (n != -99);
+
+      //Banderas
+      Console.WriteLine("Uso de Banderas");
+      bool bandera = false;
+      do
+      {
+         Console.WriteLine(" Ingrese un numero positivo: ");
+         n = int.Parse(Console.ReadLine());
+
+         if (n <= 0)
+            bandera = true;
+      } while (bandera == false);
+
       Console.ReadKey();
       }
    }
