20927ef1bcb6fc7b5a8caa4a983d2c270abb3b67
Kristel Geraldine Villalta Porras <158532211+KrisGerald02@users.noreply.github.com>
2024-09-07 17:58:45 -0600

    Update Break_DoWhile.cs

--- Metodologia de Programacion Estructurada II Semestre/Break_DoWhile.cs
+++ Metodologia de Programacion Estructurada II Semestre/Break_DoWhile.cs
@@ -1,47 +1,45 @@
 using System;
 
-   public class Program
-   {
-      public static void Main(string[] args)
-      {
-      int n;
-      char resp = ' '; 
-      Console.WriteLine("Uso de Entrada con Iteraciones (Si/No)");
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int n;
+        char resp = ' '; 
+        Console.WriteLine("Uso de Entrada con Iteraciones (Si/No)");
 
-      do
-      {
-         Console.WriteLine("Ingrese el numero: ");
-         n = int.Parse(Console.ReadLine());
+        do
+        {
+            Console.WriteLine("Ingrese el numero: ");
+            n = int.Parse(Console.ReadLine());
 
-         Console.WriteLine("Desea ingresar otro numero (S= Si || N= No): ");
-         resp = char.Parse(Console.ReadLine());
-         if (resp == 'n' || resp == 'N')
-          break;
+            Console.WriteLine("Desea ingresar otro numero (S= Si || N= No): ");
+            resp = Console.ReadLine().ToUpper()[0]; // Convertimos a mayúscula y tomamos el primer carácter
+            if (resp == 'N')
+                break;
 
-       
+        } while (resp == 'S');
 
-      } while (resp == 's' || resp == 'S');
+        // Centinela
+        Console.WriteLine("Uso de Centinela");
+        do
+        {
+            Console.WriteLine("Ingrese un numero - (Digite -99 para finalizar): ");
+            n = int.Parse(Console.ReadLine());
+        } while (n != -99);
 
-      //Centinela
-      Console.WriteLine("Uso de Centinela");
-      do
-      {
-         Console.WriteLine(" Ingrese un numero - (Digite -99 para finalizar): ");
-         n = int.Parse(Console.ReadLine());
-      } while (n != -99);
+        // Banderas
+        Console.WriteLine("Uso de Banderas");
+        bool bandera = false;
+        do
+        {
+            Console.WriteLine("Ingrese un numero positivo: ");
+            n = int.Parse(Console.ReadLine());
 
-      //Banderas
-      Console.WriteLine("Uso de Banderas");
-      bool bandera = false;
-      do
-      {
-         Console.WriteLine(" Ingrese un numero positivo: ");
-         n = int.Parse(Console.ReadLine());
+            if (n <= 0)
+                bandera = true;
+        } while (!bandera);
 
-         if (n <= 0)
-            bandera = true;
-      } while (bandera == false);
-
-      Console.ReadKey();
-      }
-   }
+        Console.ReadKey();
+    }
+}
