481ce710f1e940fecc654df259fe88a62313a5ca
Kristel Geraldine Villalta Porras <158532211+KrisGerald02@users.noreply.github.com>
2024-09-19 20:22:28 -0600

    Create Notas_EstudiantesFor.cs

---
+++ Metodologia de Programacion Estructurada II Semestre/Notas_EstudiantesFor.cs
@@ -0,0 +1,13 @@
+/* Ejercicio (Arreglos Paralelos): Se desea desarrollar un programa que registre los 
+nombres de n estudiantes y sus notas en n evaluaciones (3) de la asignatura 
+Metodología y Programación Estructurada. El programa debe:
+1. Pedir al usuario la cantidad de estudiantes. Trabajar con no menos de tres 
+estudiantes.
+2. Crear un arreglo unidimensional para almacenar los nombres de los estudiantes.
+3. Crear una matriz bidimensional para almacenar las notas de los estudiantes en 
+3 exámenes.
+4. Calcular el promedio de notas de cada estudiante.
+5. Imprimir el nombre de cada estudiante, sus 3 notas y el promedio, indicando si 
+aprobó o no el curso. Se considera aprobado, si el promedio es igual o mayor a 
+70. Imprimir cuál fue el estudiante con mayor nota final.
+Nota: Para cualquier iteración, utilice sólo la instrucción for*/
