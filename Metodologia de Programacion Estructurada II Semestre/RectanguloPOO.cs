811483fa7e0fa951bc1d443822b59963b22ae7dd
Kristel Geraldine Villalta Porras <158532211+KrisGerald02@users.noreply.github.com>
2024-09-28 23:35:33 -0600

    Update RectanguloPOO.cs

--- Metodologia de Programacion Estructurada II Semestre/RectanguloPOO.cs
+++ Metodologia de Programacion Estructurada II Semestre/RectanguloPOO.cs
@@ -12,7 +12,7 @@ public struct Rectangulo
 
     public double calcularArea()
     {
-        return (Ancho*Altura);
+        return (Ancho * Altura);
     }
 }
 
@@ -20,25 +20,25 @@ public class Geometria
 {
     public static void Main()
     {
-        Console.WriteLine("Ingrese la cantidad de rectangulos a procesar:");
-        int cant = int.Parse(Console.ReadLine()); 
-        Rectangulo[] rectangulo=new Rectangulo[cant];
-        for(int i = 0; i < rectangulo.Length; i++)
+        Console.Write("Ingrese la cantidad de rectangulos a procesar:");
+        int cant = int.Parse(Console.ReadLine());
+        Rectangulo[] rectangulo = new Rectangulo[cant];
+        for (int i = 0; i < rectangulo.Length; i++)
         {
-            Console.Write("Datos de los rectangulos");
+            Console.WriteLine("Datos de los rectangulos");
             Console.Write("Ancho: ");
-            double ancho= double.Parse(Console.ReadLine());
+            double ancho = double.Parse(Console.ReadLine());
             Console.Write("Altura: ");
-            double altura= double.Parse(Console.ReadLine());
+            double altura = double.Parse(Console.ReadLine());
 
             rectangulo[i] = new Rectangulo(ancho, altura);
         }
 
-        for(int i = 0;i < rectangulo.Length;i++)
+        for (int i = 0; i < rectangulo.Length; i++)
         {
             Console.Write("Datos de los rectangulos");
-            double area= rectangulo[i].calcularArea();
-            Console.WriteLine($"Rectangulo {i+1}: Ancho:{rectangulo[i].Ancho}, Altura:  {rectangulo[i].Altura}, Area: {area}");
+            double area = rectangulo[i].calcularArea();
+            Console.WriteLine($"Rectangulo {i + 1}: Ancho:{rectangulo[i].Ancho}, Altura:  {rectangulo[i].Altura}, Area: {area}");
         }
         Console.WriteLine();
         Console.ReadKey();
