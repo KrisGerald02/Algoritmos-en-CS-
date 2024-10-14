07f399cbce03cd400901a93f3024eaed299868e2
Kristel Geraldine Villalta Porras <158532211+KrisGerald02@users.noreply.github.com>
2024-10-14 09:53:23 -0600

    Update AreaTPPReferencia.cs

--- Metodologia de Programacion Estructurada II Semestre/AreaTPPReferencia.cs
+++ Metodologia de Programacion Estructurada II Semestre/AreaTPPReferencia.cs
@@ -1,1 +1,22 @@
+public class AreaT
+{
+   public static void Main(string[] args)
+   {
+      Console.WriteLine("Ingrese el valor de la base del triangulo: ");
+      double baset = Convert.ToDouble(Console.ReadLine());
+      Console.WriteLine("Ingrese el valor de la altura del triangulo: ");
+      double alturat = Convert.ToDouble(Console.ReadLine());
 
+      double res = 0;
+      Area(ref baset,ref alturat, ref res);
+      Console.WriteLine($"El resultado del calculo del area del triangulo es: {res}");
+      Console.WriteLine();
+      Console.ReadLine();
+   }
+
+   public static void Area(ref double x,ref double y, ref double res)
+   {
+      res= ((x * y) / 2);
+   }
+
+}
